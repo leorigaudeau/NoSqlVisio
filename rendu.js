1468229b6b5a9276e0fbf4638bc8b8947aad6925
Léo Rigaudeau <33526057+leorigaudeau@users.noreply.github.com>
2020-03-23 11:30:29 +0100

    Update rendu.js

--- rendu.js
+++ rendu.js
@@ -1,3 +1,4 @@
+// INSERT : 
 db.sport.insert({"nom":"Gieko","sportFav":"Football","division":"N1", "president":"Grégory Luxin"})
 
  db.sport.insert({"nom":"BBQ","sportFav":"Football","division":"Ligue2", "president":"matteo"})
@@ -7,14 +8,25 @@ db.sport.insert({"nom":"Gieko","sportFav":"Football","division":"N1", "president
 db.sport.insert({"nom":"XE","sportFav":"Football","division":"d7", "president":"GeoX"})2
 
 
+//ajout des champs supplementaires :
+ db.sport.updateMany({},{$set:{title :"" ,requiredTeam:false}},{upsert: true})
+
+
+// AJOUT UN miniPayer POUR LES :'requiredTeam:true'
+db.sport.updateMany({requiredTeam:true},{$set:{miniPayer:10}},{upsert: true})
+
+//incrementation de 10 pour tout les champs minPlayer et requiredTeam:true
+
 db.sport.updateMany({requiredTeam:true},{$inc:{miniPayer:10}},{upsert: true})
 
-
+// ajout Array pour tous le mondes 
 
  db.sport.updateMany({},{$set:{team:[]}},{upsert: true})
 
-
+// Ajout de deux joueurs identiques
 
 db.sport.updateMany({},{$push:{team:{"name":"pierre",playerNumber:"5"}}},{upsert: true});
+
+// ajout champs isCapitaine pour tous le mondes
 
 db.sport.updateMany({},{$push:{team:{$each:[{isCapitain:true}]}}},{upsert: true});
